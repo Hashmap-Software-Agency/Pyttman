03e046e3e8e0fcdb5e9afd415ee4e88c662074f8
Simon Olofsson <dotchetter@protonmail.ch>
2021-04-30 12:43:55 +0200

    Working install config for setuptools

---
+++ pytman/tools/pytmancli/__init__.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import argparse
+import shutil
+from datetime import datetime
+
+from py7zr import unpack_7zarchive
+
+from pathlib import Path
+
+
+class TerraFormer:
+    """
+    Terraform a directory to start developing
+    a Pytman project.
+    """
+
+    def __init__(self, app_name: str, source=None):
+
+        self.app_name = app_name
+        self.extraction_dir = Path.cwd() / Path(app_name)
+
+        application_path = Path(os.path.dirname(os.path.abspath(__file__)))
+
+        if source is None:
+            print(application_path.parent.parent.parent)
+            self.source = Path(application_path)\
+                              .parent.parent.parent / Path("project_template.7z")
+            if not os.path.isfile(self.source):
+                raise FileNotFoundError("Pytman could not locate the template "
+                                        ".7z archive for terraforming. It was "
+                                        f"expected to be here: '{self.source}'. "
+                                        "If you want, you can create this "
+                                        "structure and place the template "
+                                        f"file here, or reinstall Pytman. "
+                                        f"If you think this error is our fault - "
+                                        f"please submit an issue on GitHub!")
+        else:
+            self.source = source
+
+    def terraform(self):
+        print(f"{datetime.now()} - Creating project '{self.app_name}'...")
+        shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
+        shutil.unpack_archive(self.source, self.extraction_dir)
+        settings_file_path = Path(self.extraction_dir) / Path("settings.py")
+
+        with open(settings_file_path, "a", encoding="utf-8") as settings_file:
+            settings_file.write(f"\nAPP_NAME = \"{self.app_name}\"\n")
+        print(f"{datetime.now()} - done.")
+
+    def get_info(self):
+        return f"Extraction dir: {self.extraction_dir}, " \
+               f"Source: {self.source}"
+
+
+def run(argv=None):
+
+    if argv is None:
+        argv = sys.argv[:]
+
+    argparser = argparse.ArgumentParser(prog="Pytman CLI",
+                                        usage="%(prog)s command",
+                                        add_help=False,
+                                        allow_abbrev=False)
+    argparser.add_argument("args", nargs="*")
+
+    try:
+        command = argv[1]
+    except IndexError:
+        command = None
+
+    options, args = argparser.parse_known_args(argv[2:])
+
+    if command == "newapp":
+        app_name = options.args.pop()
+        terraformer = TerraFormer(app_name=app_name)
+        terraformer.terraform()
+    else:
+        from pytman import __version__
+        print(f"\n\nPytman CLI, version {__version__}", end="\n\n")
+        print(f"\n\nSupported commands:\n\n * -newapp [<app name>] - "
+              "Start a new app project. Creates files and "
+              "directories in the current directory")
---
+++ setup.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+# This call to setup() does all the work
+setup(
+    name="pytman",
+    version="1.0.0",
+    description="The virtual assistant framework made for developers with ideas",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dotchetter/Pytman",
+    author="Simon Olofsson",
+    project_urls={
+        "Bug Tracker": "https://github.com/dotchetter/Pytman/issues",
+    },
+    author_email="dotchetter@protonmail.ch",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Microsoft :: Windows :: Windows 10"
+    ],
+    python_requires=">=3.8",
+    packages=find_packages(),
+    include_package_data=True,
+    data_files= [('', ['pytman/core/terraform_template/project_template.7z'])],
+    install_requires=[
+        "multidict",
+        "python-dotenv",
+        "pytz",
+        "requests",
+        "py7zr"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pytman-cli=pytman.tools.pytmancli:run",
+        ]
+    },
+)
+
+
