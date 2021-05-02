7f1d18f50aa140145d242deea3b96369c23b870a
Simon Olofsson <dotchetter@protonmail.ch>
2021-05-02 22:58:52 +0200

    version rollback

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="Pyttman",
-    version="0.0.2",
+    version="0.0.0",
     description="The virtual assistant framework made for developers with ideas",
     long_description=README,
     long_description_content_type="text/markdown",
