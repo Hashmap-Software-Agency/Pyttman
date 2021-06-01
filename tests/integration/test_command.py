6cae70cadccce9269e04f19a022274e4e8f6325e
Simon Olofsson <dotchetter@protonmail.ch>
2021-06-01 17:25:07 +0200

    Integration tests for the Command, Identifier and Parser classes working together.

---
+++ tests/integration/test_command.py
@@ -0,0 +1,80 @@
+import unittest
+from datetime import datetime
+
+from pyttman import Feature
+from pyttman.core.communication.command import Command
+from pyttman.core.communication.models.containers import Reply, Message
+from pyttman.core.parsing import parsers
+from pyttman.core.parsing.identifiers import DateTimeFormatIdentifier
+
+
+class SetTimeFormat(Command):
+    description = "Sets the format of datetime outputs"
+    lead = ("set",)
+    trail = ("datetime", "format",)
+
+    class InputStringParser:
+        datetime_format = parsers.ValueParser(identifier=DateTimeFormatIdentifier)
+
+    def respond(self, messsage: Message) -> Reply:
+        if datetime_format := self.query_strings.get("datetime_format"):
+            return Reply(f"Set datetime format to: {datetime_format}")
+
+
+class GetLastItem(Command):
+    lead = ("last",)
+
+    class InputStringParser:
+        last_word = parsers.PositionalParser()
+
+    def respond(self, messsage: Message) -> Reply:
+        return Reply(f"The last value was: "
+                     f"{self.query_strings.get('last_word')}")
+
+
+class GetTime(Command):
+    description = "Returns the current time"
+    lead = ("what",)
+    trail = ("time",)
+
+    def respond(self, messsage: Message) -> Reply:
+        timestr = datetime.now().strftime(self.query_strings.get("datetime_format"))
+        return Reply(f"The time is currently {timestr}")
+
+
+class ClockFeature(Feature):
+    """
+    A basic, simple feature which
+    answers what time it is.
+    """
+    commands = (GetTime, SetTimeFormat)
+
+
+class TestCommand(unittest.TestCase):
+
+    def test_value_parser(self):
+
+        msg = Message("Set datetime format to %y-%m-%d")
+
+        set_time_command = SetTimeFormat()
+
+        if set_time_command.matches(msg):
+            set_time_command.parse_for_query_strings(msg)
+
+        print(set_time_command.query_strings)
+        self.assertEqual(set_time_command.query_strings.get("datetime_format"),
+                         "%y-%m-%d")
+        print(set_time_command.generate_help())
+        print(set_time_command.respond(messsage=msg).as_str())
+
+    def test_positional_parser(self):
+
+        get_last_item = GetLastItem()
+
+        msg = Message("the last item is turtle")
+        get_last_item.parse_for_query_strings(msg)
+
+        print(get_last_item.query_strings)
+        self.assertEqual(get_last_item.query_strings.get("last_word"), "turtle")
+
+        print(get_last_item.respond(messsage=msg).as_str())
