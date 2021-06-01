5c7a4d4dc37be483cf4f0b56d26d061d288927a7
Simon Olofsson <dotchetter@protonmail.ch>
2021-06-01 17:14:53 +0200

    Relates to #14
    Identifier class
    
    The Identifier class is an optional argument to a Parser subclass inside
    the inner InputStringParser class used in Command subclasses.
    
    It offers a simple interface for developers to find and extract strings
    in messages, for different purposes. Many times, when trying to extract
    strings of value such as in a question with metadata in it, having
    prefix and suffix words to look for or after is not enough.
    Sometimes the pattern of the value itself is more valuable.
    
    The Identifier class aims to provide an intuitive and simple API for
    these scenarios. By providing an Identifier class to a field inside a
    InputStringParser class, the InputStringParser will use that Identifier
    to fill that field.
    
    Identifier classes are available to find phone numbers, integers, and
    can be easily subclassed by writing a regex pattern to identify exactly
    what the developer is looking for in that very command.

---
+++ pyttman/core/parsing/identifiers.py
@@ -0,0 +1,69 @@
+import re
+from typing import Optional
+
+from pyttman.core.communication.models.containers import Message
+
+
+class Identifier:
+    """
+    Base class for an Identifier.
+
+    The Identifier class aids in identifying
+    strings based on their patterns, as in
+    if they contain integers, special characters,
+    whether they are capitalized, have a date-like
+    format to them, and alike.
+
+    Subclasses can define very granularly how
+    a string can look by strict rules, or less
+    so with less defined criterias.
+
+    The Identifier will use regex to assess
+    the similarity of given string.
+    """
+    patterns = (r"^.*$",)
+
+    def __init__(self):
+        self.matches: bool = False
+        self.matching_string = None
+        try:
+            self.patterns = tuple([re.compile(pat) for pat in self.patterns])
+        except Exception as e:
+            raise AttributeError("Identifier pattern could not compile") from e
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(" \
+               f"patterns={self.patterns}, " \
+               f"matches={self.matches}, " \
+               f"matching_string={self.matching_string})"
+
+    def get_matching_string(self, message: Message) -> Optional[str]:
+        """
+        Evaluates if any element in the content of
+        a Message object matches with its pattern.
+
+        :return bool: One of the patterns defined in
+        self.patterns matched a string in Message.content
+        """
+        for pattern in self.patterns:
+            for elem in message.content:
+                if re.match(pattern, elem):
+                    self.matching_string = elem
+                    return elem
+        return None
+
+
+class CellPhoneNumberIdentifier(Identifier):
+    """ Identifies whether a string is similar to a datetime """
+    patterns = (r"^(\d{3}.\d{4}.\d{3})|(\d{10})|(\d{3}.\d{3}.\d{4})$",)
+
+
+class DateTimeStringIdentifier(Identifier):
+    """ Identifies whether a string looks like a date string """
+    patterns = (r"^(\d{4}).(\d{2}).(\d{2}).(\d{2}):(\d{2}).*$",
+                r"^(\d{2}).(\d{2}).(\d{2}).(\d{2}):(\d{2}).*$")
+
+
+class DateTimeFormatIdentifier(Identifier):
+    """ identifies a datetime format configuration string """
+    patterns = (r"^%.*$",)
