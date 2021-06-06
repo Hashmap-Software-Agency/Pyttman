40892ed08c013934fe044551d8f045893cb1a144
Simon Olofsson <dotchetter@protonmail.ch>
2021-06-06 04:05:22 +0200

    Runner refactored

--- pyttman/tools/pyttmancli/executor.py
+++ pyttman/tools/pyttmancli/executor.py
@@ -16,14 +16,17 @@ class Runner:
         try:
             self.app_settings = import_module(f"{app_name}.settings")
         except ModuleNotFoundError:
-            raise ModuleNotFoundError("Pyttman cannot find the settings file for an app called "
-                                      f"'{app_name}' in the current directory. Remember to run "
-                                      f"pyttman-cli run <appname> in the parent folder of your "
-                                      f"app.")
+            raise ModuleNotFoundError("Pyttman cannot find the settings "
+                                      f"file for an app called '{app_name}' "
+                                      f"in the current directory. Remember to run "
+                                      f"pyttman-cli run <appname> in the parent "
+                                      f"folder of your app.")
 
     def run(self):
+
         import pyttman
         pyttman.load_settings(self.app_settings)
+        pyttman.is_configured = True
 
         from pyttman.core.parsing.routing import LinearSearchFirstMatchingRouter
         router = LinearSearchFirstMatchingRouter()
