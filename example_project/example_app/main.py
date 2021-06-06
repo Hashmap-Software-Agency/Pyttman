77a0be49fa7aa685bece8dcbc0360eb6696fd9c7
Simon Olofsson <dotchetter@protonmail.ch>
2021-06-06 03:36:13 +0200

    Example project updated with router and examples.

--- example_project/example_app/main.py
+++ example_project/example_app/main.py
@@ -2,7 +2,7 @@ import pyttman
 import pyttman.core.communication.command
 import pyttman.core.communication.models.containers
 from features.clockfeature import ClockFeature
-from features.schedulingfeature import SchedulingFeature
+from pyttman.core.parsing.routing import LinearSearchFirstMatchingRouter
 
 # These two steps are important for Pyttman to work properly for your app
 import settings
@@ -11,12 +11,12 @@ pyttman.load_settings(settings)
 # This shows a small exmaple of how you create a CommandProcessor,
 # provide it with the features you want to use, then give it a spin!
 processor = pyttman.CommandProcessor()
-processor.features = (SchedulingFeature(), ClockFeature())
 message = pyttman.core.communication.models.containers.Message()
 
 if __name__ == "__main__":
-
+    router = LinearSearchFirstMatchingRouter()
+    router.features = (ClockFeature(),)
     while 1:
         message.content = input("Write something! -> ")
-        result = processor.process(message)
-        print(result.response())
+        result = router.get_reply(message)
+        print(result.as_str())
