c88c488da4325829f535dc3ea749f8f28d2dba5f
Simon Olofsson <dotchetter@protonmail.ch>
2021-06-06 02:30:41 +0200

    terraformer moved to separate file

--- pyttman/tools/pyttmancli/__init__.py
+++ pyttman/tools/pyttmancli/__init__.py
@@ -1,55 +1,8 @@
 import sys
-import os
 import argparse
-import shutil
-from datetime import datetime
 
-from py7zr import unpack_7zarchive
-
-from pathlib import Path
-
-
-class TerraFormer:
-    """
-    Terraform a directory to start developing
-    a Pyttman project.
-    """
-
-    def __init__(self, app_name: str, source=None):
-
-        self.app_name = app_name
-        self.extraction_dir = Path.cwd() / Path(app_name)
-
-        application_path = Path(os.path.dirname(os.path.abspath(__file__)))
-
-        if source is None:
-            self.source = Path(application_path).parent.parent / Path("core") / \
-                          Path("terraform_template") / Path("project_template.7z")
-            if not os.path.isfile(self.source):
-                raise FileNotFoundError("Pyttman could not locate the template "
-                                        ".7z archive for terraforming. It was "
-                                        f"expected to be here: '{self.source}'. "
-                                        "If you want, you can create this "
-                                        "structure and place the template "
-                                        f"file here, or reinstall Pyttman. "
-                                        f"If you think this error is our fault - "
-                                        f"please submit an issue on GitHub!")
-        else:
-            self.source = source
-
-    def terraform(self):
-        print(f"{datetime.now()} - Creating project '{self.app_name}'...")
-        shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
-        shutil.unpack_archive(self.source, self.extraction_dir)
-        settings_file_path = Path(self.extraction_dir) / Path("settings.py")
-
-        with open(settings_file_path, "a", encoding="utf-8") as settings_file:
-            settings_file.write(f"\nAPP_NAME = \"{self.app_name}\"\n")
-        print(f"{datetime.now()} - done.")
-
-    def get_info(self):
-        return f"Extraction dir: {self.extraction_dir}, " \
-               f"Source: {self.source}"
+from pyttman.tools.pyttmancli.executor import Runner
+from pyttman.tools.pyttmancli.terraforming import TerraFormer
 
 
 def run(argv=None):
@@ -77,6 +30,8 @@ def run(argv=None):
     elif command == "run":
         app_name = options.args.pop()
         print("you want me to run", app_name)
+        runner = Runner(app_name)
+        runner.run()
     else:
         from pyttman import __version__
         print(f"\nPyttman CLI, version {__version__}")
