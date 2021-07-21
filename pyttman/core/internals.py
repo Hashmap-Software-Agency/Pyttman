3ce6dcc9b205b2dd3e6f8122aa47b84e7a77c30f
Simon Olofsson <dotchetter@protonmail.ch>
2021-07-21 20:00:37 +0200

    Spell fix

--- pyttman/core/internals.py
+++ pyttman/core/internals.py
@@ -27,7 +27,7 @@ def is_dst(timezone: str):
     """
     method for returning a bool whether or not a timezone
     currently is in daylight savings time, useful for servers
-    that run_client systems outside of the user timezone.
+    that run systems outside of the user timezone.
     :param timezone:
         string, timezone to give pytz for the dst query.
         look up available timezones at this url:
