ad0c9c0e26c497309ffeda34dfbd64c172b63faa
Simon Olofsson <dotchetter@protonmail.ch>
2021-09-01 14:27:58 +0200

    Improved unittest for the Identifier class.

--- tests/unit/test_identifiers.py
+++ tests/unit/test_identifiers.py
@@ -1,25 +1,32 @@
+import unittest
 from unittest import TestCase
 
 from pyttman.core.communication.models.containers import Message
 from pyttman.core.parsing import identifiers
+from pyttman.core.parsing.entity import Entity
 
 
 class TestDateStringIdentifier(TestCase):
 
     def setUp(self) -> None:
-        self.invalid_strings = ("2432-66-f3 0033",
-                                "1999-001-21:20:40:50",
-                                "1998/09/13-f3:00")
-
-        self.valid_strings = ("2021-07-25-09:00",
-                              "2021/07/31-09:00")
+        self.sample_message = Message("The biggest manmade disaster in human history "
+                                      "occurred 1984/04/26-01:23:45 when the Chernobyl "
+                                      "powerplant meltdown accident. Another big human "
+                                      "made disaster is the hiroshima bomb which dropped "
+                                      "over the city of Heroshima in Japan on 1945-08-06.")
 
     def test_identify_valid(self):
-        for string in self.valid_strings:
-            identifier = identifiers.DateTimeStringIdentifier()
-            self.assertEqual(string, identifier.try_identify_entity(Message(string)))
+        identifier = identifiers.DateTimeStringIdentifier(start_index=0)
 
-    def test_identify_invalid(self):
-        for string in self.invalid_strings:
-            identifier = identifiers.DateTimeStringIdentifier()
-            self.assertIsNone(identifier.try_identify_entity(Message(string)))
+        # Expect to find an Entity with the following value on index 8
+        expected_entity = Entity("1984/04/26-01:23:45", 8)
+        found_entity = identifier.try_identify_entity(self.sample_message)
+        self.assertEqual(expected_entity, found_entity)
+
+    def test_identify_valid_entity_later_occurrence(self):
+        identifier = identifiers.DateTimeStringIdentifier(start_index=9)
+
+        # Expect to find an Entity with the following value on index 8
+        expected_entity = Entity("", 34)
+        found_entity = identifier.try_identify_entity(self.sample_message)
+        self.assertIsNotNone(found_entity)
