3e00bf841c5ddf203111a61f85410abad14f463d
Simon Olofsson <dotchetter@protonmail.ch>
2021-12-05 15:44:24 +0100

    removed redundant try statement; responsibility moved outside of this scope.

--- pyttman/tools/pyttmancli/executor.py
+++ pyttman/tools/pyttmancli/executor.py
@@ -14,18 +14,9 @@ class Runner:
 
     def __init__(self, app_name: str, client: BaseClient):
 
-        sys.path.insert(0, '')
+        sys.path.insert(0, "")
         self.client = client
         self.app_catalog = Path.cwd() / Path(app_name)
-
-        try:
-            self.app_settings = import_module(f"{app_name}.settings")
-        except ModuleNotFoundError:
-            raise ModuleNotFoundError("Pyttman cannot find the settings "
-                                      f"file for an app called '{app_name}' "
-                                      f"in the current directory. Remember to "
-                                      f"run pyttman-cli run <appname>"
-                                      f" in the parent folder of your app.")
 
     def run(self):
         """
