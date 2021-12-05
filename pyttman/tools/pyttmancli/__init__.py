529a140ba97aee265babb3d7b1c915bcd1794ca7
Simon Olofsson <dotchetter@protonmail.ch>
2021-12-05 15:42:27 +0100

    license added

--- pyttman/tools/pyttmancli/__init__.py
+++ pyttman/tools/pyttmancli/__init__.py
@@ -1,32 +1,34 @@
+
 import argparse
 import sys
 from typing import List
 
 from pyttman.core.parsing.routing import FirstMatchingRouter
 from pyttman.tools.pyttmancli.executor import Runner
-from pyttman.tools.pyttmancli.terraforming import TerraFormer, bootstrap_environment
+from pyttman.tools.pyttmancli.terraforming import TerraFormer, \
+    bootstrap_environment
 
-#     MIT License
+# MIT License
 #
-#      Copyright (c) 2021-present Simon Olofsson
+# Copyright (c) 2021-present Simon Olofsson
 #
-#      Permission is hereby granted, free of charge, to any person obtaining a copy
-#      of this software and associated documentation files (the "Software"), to deal
-#      in the Software without restriction, including without limitation the rights
-#      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#      copies of the Software, and to permit persons to whom the Software is
-#      furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
-#      The above copyright notice and this permission notice shall be included in all
-#      copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-#      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#      SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 """
 This file contains Intents and Ability classes which make out
@@ -225,7 +227,7 @@ def run(argv=None, dev_args: List = None):
                      strings, for unit testing the Pyttman CLI
                      or otherwise using it outside of a terminal
                      shell. Defining this argument will automatically
-                     disregarg the 'argv' arguments
+                     disregard the 'argv' arguments
     :return: None
     """
     terminal_message: Message = Message()
