3ac64494a3eef4cffc5fe07ee9da23cf26deb7f8
Simon Olofsson <dotchetter@protonmail.ch>
2022-02-20 23:18:52 +0100

    Entity class module moved. Added field: 'is_fallback_default', used when traversing EntityField instances.

--- pyttman/core/entity_parsing/entity.py
+++ pyttman/core/entity_parsing/entity.py
@@ -15,7 +15,8 @@ class Entity:
     the message object from which it stems, and its string value.
     """
     value: Any
-    index_in_message: int
+    index_in_message: int = 0
+    is_fallback_default: bool = False
 
     def __eq__(self, other):
         if isinstance(other, str):
