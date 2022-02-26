2df2f42dc4a9a81f2478f9fb5cb0a438464f2ae2
Simon Olofsson <dotchetter@protonmail.ch>
2022-02-26 19:39:00 +0100

    Added unittests

---
+++ tests/integration/entity_parsing/test_intents/test_intent_matching.py
@@ -0,0 +1,17 @@
+from core.communication.models.containers import Message
+from tests.integration.entity_parsing.base import PyttmanInternalTestBaseCase, \
+    ImplementedTestIntent
+
+
+class PyttmanInternalEntityParserTestWebscraperApp(PyttmanInternalTestBaseCase):
+    lead = ("search",)
+
+    mock_message = Message("add new purchase SomeItem 100")
+
+    class IntentClass(ImplementedTestIntent):
+        """
+        This test checks matching on a single word, only by the 'lead'
+        property.
+        """
+        lead = ("new", "add")
+        trail = ("expense", "purchase")
