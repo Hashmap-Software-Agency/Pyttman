d395e27c655ebfca2d91f1e8bbd88c5702a1173d
Simon Olofsson <dotchetter@protonmail.ch>
2022-04-06 23:49:42 +0200

    Initial commit for user test suites.

---
+++ pyttman/tools/testing/testcase.py
@@ -0,0 +1,8 @@
+from tests.core.entity_parsing.base import PyttmanInternalTestBaseCase
+
+
+class PyttmanTestCase(PyttmanInternalTestBaseCase):
+    """
+    Subclass this test case to test your intents and abilities.
+    """
+    pass
