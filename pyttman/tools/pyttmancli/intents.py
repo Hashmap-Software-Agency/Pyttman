383317693ae728f3924ad07557e22cc7f4b729ef
Simon Olofsson <dotchetter@protonmail.ch>
2022-04-15 11:39:15 +0200

    Revert "file added when creating abilities with pyttman-cli: 'models.py'"
    This reverts commit 4f384b4d7301858c86ae312ad11e89b6b70cbaa3.

--- pyttman/tools/pyttmancli/intents.py
+++ pyttman/tools/pyttmancli/intents.py
@@ -195,47 +195,5 @@ class CreateNewAbilityIntent(Intent):
         for file in files_to_create:
             rel_path = ability_catalog / file
             with open(rel_path, "w", encoding="utf-8") as f:
-                f.write("# Created by Pyttman ")
+                f.write("\n# Created by Pyttman ")
         return Reply(f"Created ability '{ability_name}'.")
-
-
-class PyttmanCli(Ability):
-    """
-    Encapsulates the Pyttman CLI tool 'pyttman'
-    used in the terminal by framework users.
-    """
-    intents = (CreateNewApp,
-               RunAppInDevMode,
-               RunAppInClientMode,
-               CreateNewAbilityIntent)
-
-    description = f"\nPyttman v{pyttman.__version__}\n\n" \
-                  "For help about a commend, type pyttman help [command]" \
-                  f"\n\nSupported commands:\n"
-
-    def before_create(self):
-        responses = {"NO_APP_NAME_MSG": "Please provide a name "
-                                        "for your app."}
-        self.storage.put("app", None)
-        self.storage.put("ready", False)
-        self.storage.put("template_url",
-                         "https://github.com/dotchetter/pyttman-"
-                         "project-template/archive/refs/heads/main.zip")
-        self.storage |= responses
-
-    def run_application(self) -> None:
-        """
-        Runs a Pyttman application with its Runner context
-        as provided.
-        :return: None
-        """
-        # noinspection PyTypeChecker, PyUnusedLocal
-        # #(used for attribute access in completion)
-        app: PyttmanApp = None
-        if (app := self.storage.get("app")) is not None:
-            print(f"- Ability classes loaded: "
-                  f"{app.client.message_router.abilities}")
-            app.start()
-        else:
-            raise RuntimeError("No Runner provided, app cannot start. "
-                               "Exiting...")
