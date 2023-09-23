8df9297b7c46c0aacd1692ef748e3963fa257a22
Simon Olofsson <simon@hashmap.se>
2023-09-23 11:49:43 +0200

    Fixed build file

--- devtools/build.py
+++ devtools/build.py
@@ -6,11 +6,13 @@ from pathlib import Path
 
 # Set the current working dir to parent directory
 sys.path.append(Path.cwd().parent.as_posix())
-os.chdir("..")
-CATALOGS = ("build", "dist")
 
+CATALOGS = ("build", "dist")
 BUILD_CMD = "python -m setup sdist bdist_wheel".split()
 
+
 if __name__ == "__main__":
-    [shutil.rmtree(i) for i in CATALOGS]
+    subprocess.Popen(BUILD_CMD)
+    [shutil.rmtree(i) for i in CATALOGS if Path(i).exists()]
     subprocess.run(BUILD_CMD)
+
