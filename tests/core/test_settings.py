b30d1abd26f0c16a7c4d289bc5f61b648e927c11
RadarJam <ostberg.rasmus@gmail.com>
2023-09-24 15:55:45 +0200

    Test suite is now using pytest test fixtures

--- tests/core/test_settings.py
+++ tests/core/test_settings.py
@@ -4,23 +4,26 @@ from pyttman.core.internals import Settings
 from importlib import import_module
 from . import mocksettings
 
-class PyttmanInternalSettingsPyttmanApp(PyttmanInternalBaseTestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        settings_names = [i for i in dir(mocksettings)
-                    if not i.startswith("_")]
-        settings_config = {name: getattr(mocksettings, name)
-                    for name in settings_names}
-        self.settings = Settings(**settings_config)        
+import pytest
 
-    def test_read_settings_with_dictionary(self):
-        
-        self.assertTrue(self.settings.d.k2.a == "a")
-        self.assertTrue(self.settings.d["k2"].a == "a")
-        self.assertTrue(self.settings.d["k2"]["a"] == "a")
-        
-        self.assertTrue(self.settings.d.k1 == "v1")
-        self.assertTrue(self.settings.d["k1"] == "v1")
+@pytest.fixture
+def mockSettings():
+    settings_names = [i for i in dir(mocksettings)
+                if not i.startswith("_")]
+    settings_config = {name: getattr(mocksettings, name)
+                for name in settings_names}
 
-        self.assertTrue(self.settings.foo == "bar")
+    return Settings(**settings_config)
+
+def test_read_settings_with_dictionary(mockSettings):
+    settings = mockSettings
+    
+    assert settings.d.k2.a == "a"
+    assert settings.d["k2"].a == "a"
+    assert settings.d["k2"]["a"] == "a"
+
+    assert settings.d.k1 == "v1"
+    assert settings.d["k1"] == "v1"
+
+    assert settings.foo == "bar"
         
