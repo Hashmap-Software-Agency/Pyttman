a9c43287e8f8ba4bae78e970d12085260ec76b89
RadarJam <ostberg.rasmus@gmail.com>
2023-09-24 16:09:01 +0200

    Test Fixture generates MockSettings

--- tests/core/test_settings.py
+++ tests/core/test_settings.py
@@ -2,28 +2,31 @@ from tests.module_helper import PyttmanInternalBaseTestCase
 from pyttman.core.internals import Settings
 
 from importlib import import_module
-from . import mocksettings
-
 import pytest
 
 @pytest.fixture
 def mockSettings():
-    settings_names = [i for i in dir(mocksettings)
-                if not i.startswith("_")]
-    settings_config = {name: getattr(mocksettings, name)
-                for name in settings_names}
+    
+    mock_settings = {
+        "d":{
+            "k1":"v1",
+            "k2":{
+                "a":"a",
+                "b":"b"
+            }
+        },
+        "foo":"bar"
+    }
 
-    return Settings(**settings_config)
+    return Settings(**mock_settings)
 
 def test_read_settings_with_dictionary(mockSettings):
-    settings = mockSettings
-    
-    assert settings.d.k2.a == "a"
-    assert settings.d["k2"].a == "a"
-    assert settings.d["k2"]["a"] == "a"
+    assert mockSettings.d.k2.a == "a"
+    assert mockSettings.d["k2"].a == "a"
+    assert mockSettings.d["k2"]["a"] == "a"
 
-    assert settings.d.k1 == "v1"
-    assert settings.d["k1"] == "v1"
+    assert mockSettings.d.k1 == "v1"
+    assert mockSettings.d["k1"] == "v1"
 
-    assert settings.foo == "bar"
+    assert mockSettings.foo == "bar"
         
