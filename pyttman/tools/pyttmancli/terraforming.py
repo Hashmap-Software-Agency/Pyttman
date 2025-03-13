f16a78be4e0cef9e644352b9ec5c7daf0eaeca80
Simon Olofsson <simon@hashmap.se>
2025-03-13 17:46:08 +0100

    add 'app' to message router when bootstrapping app

--- pyttman/tools/pyttmancli/terraforming.py
+++ pyttman/tools/pyttmancli/terraforming.py
@@ -165,10 +165,12 @@ def bootstrap_app(module: str = None, devmode: bool = False,
                    "1.3.2")
 
     # Load plugins
+    pyttman.logger.log("- Loading plugins...")
     try:
         plugins: list[PyttmanPlugin] = settings.PLUGINS
     except AttributeError as e:
         raise AttributeError("PLUGINS is missing from settings.py.") from e
+    pyttman.logger.log(f"- Plugins loaded: {[i.__class__.__name__ for i in plugins]}")
 
     message_router_class_name = message_router_config.pop()
     message_router_module = ".".join(message_router_config)
@@ -256,7 +258,7 @@ def bootstrap_app(module: str = None, devmode: bool = False,
                           plugins=plugins)
 
     # Create a log entry for app start
-    pyttman.logger.log(f" -- App {app_name} started: {datetime.now()} -- ")
+    pyttman.logger.log(f" - App {app_name} started: {datetime.now()} -- ")
     app = PyttmanApp(client=client,
                      name=settings.APP_NAME,
                      settings=settings,
@@ -266,6 +268,7 @@ def bootstrap_app(module: str = None, devmode: bool = False,
     app.abilities = load_abilities(settings)
     prepare_app(module)
     message_router.abilities = app.abilities
+    message_router.app = app
     del settings.CLIENT
     return app
 
