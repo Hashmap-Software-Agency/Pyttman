ff28a10db3328b513ff89b8aaf5a8adf89e3b50b
Simon Olofsson <simon@hashmap.se>
2025-03-13 17:38:44 +0100

    improved how plugins are executed

--- pyttman/clients/base.py
+++ pyttman/clients/base.py
@@ -1,7 +1,5 @@
 import abc
-import enum
 
-import pyttman
 from pyttman.core.containers import MessageMixin, Reply, ReplyStream
 from pyttman.core.middleware.routing import AbstractMessageRouter
 from pyttman.core.plugins.base import PyttmanPlugin
@@ -12,12 +10,6 @@ class BaseClient(abc.ABC):
     Baseclass for Clients, for interfacing
     with users directly or through an API.
     """
-    class PluginIntercept(enum.StrEnum):
-        before_intents = "before_intents"
-        after_intents = "after_intents"
-        before_app = "before_app"
-        after_app = "after_app"
-
     def __init__(self,
                  message_router: AbstractMessageRouter,
                  plugins: list[PyttmanPlugin],
@@ -46,30 +38,9 @@ class BaseClient(abc.ABC):
         """
         pass
 
-    def execute_plugins(self,
-                        intercept_point: PluginIntercept,
-                        payload: MessageMixin):
-        """
-        Executes plugin methods.
-        """
-        for plugin in self.plugins:
-            if intercept_point is self.PluginIntercept.before_intents:
-                plugin.before_intent(payload)
-            elif intercept_point is self.PluginIntercept.after_intents:
-                plugin.after_intent(payload)
-            elif intercept_point is self.PluginIntercept.before_app:
-                plugin.before_app_start(pyttman.app)
-            elif intercept_point is self.PluginIntercept.after_app:
-                plugin.after_app_stops(pyttman.app)
-
     def reply_to_message(self, message: MessageMixin) -> Reply | ReplyStream:
         """
         Wrapper for calling the message router for a reply, whilst
         calling plugin methods in order.
         """
-        self.execute_plugins(intercept_point=self.PluginIntercept.before_intents,
-                             payload=message)
-        reply = self.message_router.get_reply(message)
-        self.execute_plugins(intercept_point=self.PluginIntercept.after_intents,
-                             payload=message)
-        return reply
+        return self.message_router.get_reply(message)
--- pyttman/core/plugins/base.py
+++ pyttman/core/plugins/base.py
@@ -1,5 +1,24 @@
+import enum
+
 from pyttman.core.containers import MessageMixin
 
+
+class PyttmanPluginIntercept(enum.StrEnum):
+    """
+    Enum for the different intercepts available for
+    Pyttman plugins to hook into. The intercepts are
+    called at different stages of the application
+    runtime, allowing for custom workflows to be
+    implemented.
+    """
+    before_app_start = "before_app_start"
+    after_app_stops = "after_app_stops"
+    before_router = "before_router"
+    before_intent = "before_intent"
+    after_intent = "after_intent"
+    before_entity_extraction = "before_entity_extraction"
+    after_entity_extraction = "after_entity_extraction"
+    no_intent_match = "no_intent_match"
 
 class PyttmanPlugin:
     """
@@ -11,27 +30,55 @@ class PyttmanPlugin:
     various layers.
 
     The instance is long-lived throughout the application runtime.
+
+    To control when the plugin is allowed to run, provide a list
+    of allowed intercepts in the constructor. If no intercepts are
+    provided, the plugin will never run.
+
+    allowed_intercepts: list[PyttmanPluginIntercepts]
+        A list of allowed points of intercept, where the plugin is allowed
+        to intercept the application runtime. An empty list will allow
+        no intercepts, effectively disabling the plugin.
     """
+    def __init__(self, allowed_intercepts: list[PyttmanPluginIntercept] or None = None):
+        self.allowed_intercepts = set(allowed_intercepts) if allowed_intercepts else set()
+
+    def allowed_to_intercept_at(self, intercept: PyttmanPluginIntercept):
+        """
+        Check if the plugin is allowed to run for the given intercept.
+        """
+        return intercept in self.allowed_intercepts
 
     def before_app_start(self, app):
         """
         Implement this method to have code execute before the app
         starts.
         """
-        pass
+        return app
 
     def after_app_stops(self, app):
         """
         Implement this method to have code execute when the
         app has exited
         """
+        return app
+
+    def before_router(self, message: MessageMixin):
+        """
+        Implement this method to interact with the processing
+        of a message, before it's processed by the Router.
+        This hook is useful to manipulate the message object
+        before it's passed to the Router, affecting which
+        intent it will match.
+        """
+        return message
 
     def before_intent(self, message: MessageMixin):
         """
         Implement this method to interact with the processing
         of a message, before it's processed by the matching Intent.
         """
-        pass
+        return message
 
     def after_intent(self, reply: MessageMixin):
         """
@@ -39,4 +86,29 @@ class PyttmanPlugin:
         from an Intent, before it's delivered to the Client for
         a platform response.
         """
-        pass
+        return reply
+
+    def before_entity_extraction(self, message: MessageMixin):
+        """
+        Implement this method to interact with the processing
+        of a message, before entities are parsed from the message.
+        This is one way to affect the entities that are extracted
+        from the message, when it's been passed to the matching intent.
+        """
+        return message
+
+    def after_entity_extraction(self, message: MessageMixin):
+        """
+        This hook allows you to interfere with the message object
+        (with associated entities on message.entities), to manipulate
+        or change any entities extracted from the message before
+        it's passed to the Intent.
+        """
+        return message
+
+    def no_intent_match(self, message: MessageMixin):
+        """
+        Implement this method to interact with the message
+        when no intent matches the message.
+        """
+        return message
