320d68859b11c6a6104936fd12dc3a208a9e21e8
Simon Olofsson <simon@hashmap.se>
2025-03-13 17:40:39 +0100

    fix devtools script

--- devtools/build.py
+++ devtools/build.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import sys
 from pathlib import Path
+from time import sleep
 
 # Set the current working dir to parent directory
 sys.path.append(Path.cwd().parent.as_posix())
@@ -12,7 +13,6 @@ BUILD_CMD = "python -m setup sdist bdist_wheel".split()
 
 
 if __name__ == "__main__":
-    subprocess.Popen(BUILD_CMD)
     [shutil.rmtree(i) for i in CATALOGS if Path(i).exists()]
+    sleep(1)
     subprocess.run(BUILD_CMD)
-
